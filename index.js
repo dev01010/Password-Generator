b87166c8a7580d64fbcf006d8c953bf3d41c71a1
Frederick Arthur <ghostly.op1@gmail.com>
2023-03-27 16:44:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ let generateButton = document.querySelector(".generate-btn");
 let six = document.querySelector(".six");
 let eight = document.querySelector(".eight");
 let ten = document.querySelector(".ten");
-// let pLength = document.querySelector(".password-length");
+let pLength = document.querySelector(".password-length");
 
 let passwordLength;
 
 six.addEventListener("click", function () {
   passwordLength = 6;
-  // pLength.textContent += passwordLength;
+  pLength.textContent = "Length: " + passwordLength;
 });
 eight.addEventListener("click", function () {
   passwordLength = 8;
-  // pLength.textContent += passwordLength;
+  pLength.textContent = "Length: " + passwordLength;
 });
 ten.addEventListener("click", function () {
   passwordLength = 10;
-  // pLength.textContent += passwordLength;
+  pLength.textContent = "Length: " + passwordLength;
 });
 
 function generateRandomString(passwordLength) {
