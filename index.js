82f4641da744f0014227516b24c52f838e4ecc8f
Frederick Arthur <ghostly.op1@gmail.com>
2023-03-27 15:53:19 +0000

    A simple password Generator created with HTML, CSS and Javascript

---
+++ index.js
@@ -0,0 +1,32 @@
+let passwordEl = document.querySelector(".password");
+let generateButton = document.querySelector(".generate-btn");
+let six = document.querySelector(".six");
+let eight = document.querySelector(".eight");
+let ten = document.querySelector(".ten");
+// let pLength = document.querySelector(".password-length");
+
+let passwordLength;
+
+six.addEventListener("click", function () {
+  passwordLength = 6;
+  // pLength.textContent += passwordLength;
+});
+eight.addEventListener("click", function () {
+  passwordLength = 8;
+  // pLength.textContent += passwordLength;
+});
+ten.addEventListener("click", function () {
+  passwordLength = 10;
+  // pLength.textContent += passwordLength;
+});
+
+function generateRandomString(passwordLength) {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$&*.,";
+
+  for (let i = 0; i < passwordLength; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  passwordEl.textContent = result;
+}
